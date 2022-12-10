b4387e765be1ae4016acb80662bed1dd0ebd374a
afonsofeliciano <afonsofeliciano@live.com>
2022-12-10 15:09:31 -0300

    Implementação de método estático

--- 17 - POO/2 - Contas Bancárias.py
+++ 17 - POO/2 - Contas Bancárias.py
@@ -1,4 +1,15 @@
+from datetime import datetime
+import pytz
+
+
 class ContaCorrente:
+
+    @staticmethod
+    def _data_hora():
+        """Retorna a data e hora atual"""
+        fuso_BR = pytz.timezone("Brazil/East")
+        horario_BR = datetime.now(fuso_BR)
+        return horario_BR.strftime('%d/%m/%Y %H:%M:%S')
 
     def __init__(self, nome, cpf):
         """Inicialização da classe"""
@@ -6,6 +17,7 @@ class ContaCorrente:
         self.cpf = cpf 
         self.saldo = 0
         self.limite = None
+        self.transacoes = []
     
     def consultar_saldo(self):
         """Consulta o valor do saldo"""
@@ -14,6 +26,7 @@ class ContaCorrente:
     def depositar(self, valor):
         """Deposita um valor na conta"""
         self.saldo += valor
+        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
 
     def _definir_limite_conta(self):
         """Define o limite da conta. Método privado"""
@@ -27,10 +40,18 @@ class ContaCorrente:
             self.consultar_saldo()
         else:
             self.saldo -= valor
+            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
     
     def consulta_limite_conta(self):
         """Retorna o limite em cheque especial"""
         print("O seu limite de cheque especial é de {}".format(self._definir_limite_conta()))
+
+    def consultar_historico_transacoes(self):
+        """Consulta o histórico de transações"""
+        print("Histórico de transações")
+        print("Valor, Saldo, Data e Hora")
+        for transacao in self.transacoes:
+            print(transacao)
 
     
 #Programa
@@ -55,3 +76,5 @@ conta_Afonso.consultar_saldo()
 #Consultando limite da conta
 conta_Afonso.consulta_limite_conta()
 
+print("\n")
+conta_Afonso.consultar_historico_transacoes()
