95ee91c9c8dae941b4a05ff457847eca69f2ef77
afonsofeliciano <afonsofeliciano@live.com>
2022-12-10 09:50:28 -0300

    Criando uma classe tv

---
+++ 17 - POO/1 - Criando classe.py
@@ -0,0 +1,21 @@
+
+
+class TV:
+
+    def __init__(self):
+        self.cor = 'preta'
+        self.ligada = False
+        self.tamanho = 55
+        self.canal = 'SBT'
+        self.volume = 10
+
+tv_sala = TV()
+tv_quarto = TV()
+
+
+tv_sala.cor = 'branca'
+
+print("Cor tv sala: ",tv_sala.cor)
+print("Cor tv quarto: ", tv_quarto.cor)
+    
+    
