a8e009d0cd22bad38ebfc85803c701db3f7e96d2
afonsofeliciano <afonsofeliciano@live.com>
2022-12-10 14:12:15 -0300

    Passando parâmetros nos métodos

--- 17 - POO/1 - Criando classe.py
+++ 17 - POO/1 - Criando classe.py
@@ -9,8 +9,10 @@ class TV:
         self.canal = 'SBT'
         self.volume = 10
     
-    def mudar_canal(self):
-        self.canal = 'Band'
+    def mudar_canal(self, novo_canal):
+        """Muda o canal da tv"""
+        self.canal = novo_canal
+        print("Canal alterado para {}".format(novo_canal))
         
 
 tv_sala = TV()
@@ -24,7 +26,7 @@ print("Cor tv quarto: ", tv_quarto.cor)
 print("######")
 
 print("Canal da tv sala:", tv_sala.canal)
-tv_sala.mudar_canal()
+tv_sala.mudar_canal("Disney Plus")
 print("Canal da tv sala após mudar canal:", tv_sala.canal)
 
     
